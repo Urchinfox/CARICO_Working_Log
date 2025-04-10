ba55e651bac179e3b609a197ec4b49b225e1c55b
Daniel <davidtea0427@gmail.com>
2025-04-10 17:00:35 +0800

    set route

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
   //     },
   //   ],
   // },
-  base: process.env.NODE_ENV === 'production' ? '/Carico-workingLog/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/CARICO_Workout_Log/' : '/',
   plugins: [react()],
 })
